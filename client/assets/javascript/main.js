6b7a84da797c1152e6d82e75d749b1b5f86cfae3
javad <j.bagirov@expressbank.az>
2025-10-18 22:44:47 +0400

    if email input is null then forward index page

--- client/assets/javascript/main.js
+++ client/assets/javascript/main.js
@@ -2,11 +2,11 @@ console.log("main started")
 
 function main(){
     let emailInput = document.querySelector("#email-lan-page");
-    if(emailInput){
+    if(emailInput.value!=null && emailInput.value.length>1){
         window.localStorage.setItem("guest_email",emailInput.value);
         window.location.href = "client/pages/register.html"
     }else{
         emailInput.style = "border: 1px solid red;"
-        window.location.href = "client/pages/index.html"
+        window.location.href = "index.html"
     }
 }
